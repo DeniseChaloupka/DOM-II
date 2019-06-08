7200dd6e7649f393922501d4945a0850a9570ccf
Denise Chaloupka <denisechaloupka@yahoo.com>
2019-06-07 20:52:29 -0500

    Secret message

--- js/index.js
+++ js/index.js
@@ -8,3 +8,13 @@ navAnchors.addEventListener('mouseover', (event) => {
     
 });
 
+//Key Down / Press any key for a secret message for any plagerizers
+const pageBody = document.querySelector('body');
+const noPlagerismFont = document.querySelector('.footer p');
+
+pageBody.addEventListener('keydown', (event) => {
+    noPlagerismFont.textContent = 'Copyright Fun Bus 2019. https://www.copyright.gov/help/faq/definitions.html';
+    console.log('alerted plagerizers')
+
+});
+
