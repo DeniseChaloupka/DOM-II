c16d89cd603ec624f35b9ee2c8d0b8cc872943e7
Denise Chaloupka <denisechaloupka@yahoo.com>
2019-06-07 20:50:35 -0500

    mouse over added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+//Mouse Over / changes anchor colors
+const navAnchors = document.querySelector('.nav');
+
+navAnchors.addEventListener('mouseover', (event) => {
+    navAnchors.style.backgroundColor = 'yellow';
+    console.log('gold nav bar')
+    
+});
+
